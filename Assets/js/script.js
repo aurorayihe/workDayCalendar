45ff084edf6bc584014b8e89a153363ea73941e4
JiongW <jiongw22@gmail.com>
2022-08-07 20:09:38 -0700

    complete all functions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,8 @@
 var timeDisplayEl = $('#currentDay');
 var textEl = $('.textarea');
+var timeEl = $('.hour');
+var saveBtnEl = $('.saveBtn');
 
-console.log(textEl);
-textEl[0].setAttribute('class', 'future col-9 textarea');
 // Handle displaying the time
 function displayTime(){
     var currentDate = moment().format('dddd, MMMM D');
@@ -11,10 +11,60 @@ function displayTime(){
 
 setInterval(displayTime, 1000);
 
+
 // Handle the color of each text area
-/*
 function setColor(){
     var currentTime = moment().format('H');
-    textEl.children(1).addClass("future");
+    for (let i = 0; i < textEl.length; i++){
+        var time = timeEl[i].getAttribute("data-time");
+        if (currentTime < time){
+            textEl[i].setAttribute('class', 'future col-9 textarea');
+        } else if (currentTime == time){
+            textEl[i].setAttribute('class', 'present col-9 textarea');
+        } else {
+            textEl[i].setAttribute('class', 'past col-9 textarea');
+        }
+    }
 }
-setColor();*/
+
+setColor();
+// handle to do submission
+var content = [];
+var times = ["09", "10", "11", "12", "13", "14","15","16","17"];
+saveBtnEl.on("click", saveContent);
+
+function saveContent(event){
+    content = JSON.parse(localStorage.getItem("content"));
+    var btnClicked = $(event.target);
+    var time = btnClicked[0].getAttribute("data-time");
+    var input = btnClicked.parent().find("input").val();
+    if (input == ""){
+        return;
+    }
+    else{
+        btnClicked.parent().find("input").val(input);
+        content.push({clickTime:time, textInput: input});
+        storeLocal();
+        renderInput();
+    }
+
+}
+
+function storeLocal(){
+    localStorage.setItem("content", JSON.stringify(content));
+}
+
+function renderInput(){
+    var currentContentJSON = localStorage.getItem("content");
+    var currentContent=JSON.parse(currentContentJSON);
+    console.log(currentContent);
+    for(let i = 0; i < currentContent.length; i++){
+        var currentItem = currentContent[i];
+        let input = currentItem.textInput;
+        let j = times.indexOf(currentItem.clickTime);
+        textEl = $('.textarea'); 
+        textEl[j].setAttribute('value', input);
+    }
+}
+
+renderInput();
